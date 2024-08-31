258dc6b357180a00d20c4f51ff5d39903bd1f7fd
Daniel Boros <dancixx@gmail.com>
2024-08-31 22:58:45 +0200

    fix: meta data

--- src/post.rs
+++ src/post.rs
@@ -15,37 +15,24 @@ pub fn Component() -> impl IntoView {
 
     view! {
         <Suspense fallback=|| ()>
-            <Title text=post.get().unwrap_or_default().title.to_string() />
-            <Show when=move || post.get().is_some() fallback=|| view! { <></> }>
-                <Meta
-                    name="description"
-                    content=post.get().unwrap_or_default().summary.to_string()
-                />
-                <Meta
-                    property="og:title"
-                    content=post.get().unwrap_or_default().title.to_string()
-                />
-                <Meta
-                    property="og:description"
-                    content=post.get().unwrap_or_default().summary.to_string()
-                />
-            </Show>
             <Meta property="og:type" content="article" />
             <Meta property="og:url" content=format!("https://rust-dd.com/post/{}", slug()) />
             <Meta property="og:image" content="https://static.rust-dd.com/rust-dd.png" />
-            {post
-                .get()
-                .unwrap_or_default()
-                .tags
-                .into_iter()
-                .map(|tag| {
-                    view! { <Meta name="keywords" content=tag.to_string() /> }
-                })
-                .collect::<Vec<_>>()}
             {move || {
                 post.with(|post| {
                     let post = post.clone().unwrap_or_default();
                     view! {
+                        <Title text=post.title.to_string() />
+                        <Meta name="description" content=post.summary.to_string() />
+                        <Meta property="og:title" content=post.title.to_string() />
+                        <Meta property="og:description" content=post.summary.to_string() />
+                        {post
+                            .tags
+                            .into_iter()
+                            .map(|tag| {
+                                view! { <Meta name="keywords" content=tag.to_string() /> }
+                            })
+                            .collect::<Vec<_>>()}
                         <article>
                             <div class="flex flex-col gap-4 mx-auto max-w-3xl">
                                 <p class="text-4xl font-semibold">{post.title.clone()}</p>
