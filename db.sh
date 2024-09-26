d20364d4367a84def1d45713be1a422ceaf8f449
Daniel Boros <dancixx@gmail.com>
2024-09-26 18:24:33 +0200

    feat: add header image

--- db.sh
+++ db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-env SURREAL_EXPERIMENTAL_GRAPHQL=true surreal start --auth --log trace --user root --pass root --bind 127.0.0.1:8000 file:rustblog.db
+env SURREAL_EXPERIMENTAL_GRAPHQL=true surreal start --log trace --user root --pass root --bind 127.0.0.1:8000 surrealkv:rustblog.db
