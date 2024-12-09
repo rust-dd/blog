ea47dd04f5debd7a7c240c5dd7f5d694d24762f1
Daniel Boros <dancixx@gmail.com>
2024-12-09 19:20:28 +0100

    feat: use local fonts

--- src/app.rs
+++ src/app.rs
@@ -77,10 +77,6 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <Meta name="twitter:image:alt" content="Rust-DD Framework" />
                 <Link rel="preconnect" href="https://fonts.googleapis.com" />
                 <Link rel="preconnect" href="https://fonts.gstatic.com" />
-                <Link
-                    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-                    rel="stylesheet"
-                />
             </head>
             <body class="bg-[#1e1e1e]">
                 <App />
